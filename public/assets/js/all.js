16a4555d02b1a01b4cb861e4a592d583874309d2
fabik111 <fabiomassimo.centonze@mail.polimi.it>
2018-06-13 19:31:38 +0200

    add all_serv method

--- public/assets/js/all.js
+++ public/assets/js/all.js
@@ -38,10 +38,12 @@ function loadData(json){
     var el="";
     el += '<div class="services block block-bg-gradient"><div class="container">'
     var ph="";
-    if(URL.table!="services")
-        ph="1.jpg";
+    var link="services/service.html?id=";
+    if(URL.table!="services"){
+       link="location/location.html?id=";
+        ph="1.jpg";}
     for(var i = 0; i< json.length; i++){
-        var locLink = 'location/location.html?id=' + json[i].id;
+        var locLink = link + json[i].id;
         var locImg = json[i].img;
         el += '<div class="row"><div class="col-md-7"><a href="' + locLink + '"><img  class="img-fluid rounded mb-3 mb-md-0" src="'+ locImg + ph+'" height="60%" width="60%" alt="' + json[i].name + '></a></div><div class="col-md-5"><h3><b>' +json[i].name + '</b></h3> <a class="btn btn-primary" href="' + locLink + '">Scopri di più</a></div></div>';
         
