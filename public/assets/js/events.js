0ef502f3a1a4aa80e8c53fd5fb72d3a4def8e7f0
baldasseva <eva.baldasseroni@mail.polimi.it>
2018-06-13 15:56:57 +0200

    Location + people + servizi JS (inizio event)

--- public/assets/js/events.js
+++ public/assets/js/events.js
@@ -1,26 +1,21 @@
 var page_title;
-var loc_title;
+var event_title;
 var contenuto;
 var image_url;
-var map_url;
-var contatti;
 
 $(document).ready(function(){
     
     page_title = $("title");
-    loc_title = $("#title_loc");
-    contenuto = $("#contenuto_loc");
-    image_url = $("#image_loc");
-    map_url = $("iframe");
-    contatti = $("#contacts");
-    
+    event_title = $("#title_event");
+    contenuto = $("#contenuto_event");
+    image_url = $("#image_event");
      console.log(URL.id);
     $.ajax({
         method: "GET",
         //dataType: "json",
         dataType: 'json',
         crossDomain: true,
-        url: "http://localhost:3000/location/"+URL.id,
+        url: "http://localhost:3000/events/"+URL.id,
         data: {
             id: URL.id //SEND THE ID OF THE SERVICE TO THE SERVER TO RETRIVE ONLY THOSE DATA
         },
@@ -30,33 +25,11 @@ $(document).ready(function(){
             
             //UPDATE TEXT AND HTML RELATED TO THE RESPONSE RECEIVED
             page_title.text(response.name);
-            loc_title.text(response.name);
+            event_title.text(response.name);
             contenuto.html(response.descrizione);
-            image_url.attr("src",response.img + "intro.jpg"); 
-            map_url.attr("src", response.mappa);
-            contatti.html(response.contatti);
-            createGallery(response.img);
+            image_url.attr("src", response.img);
             
         },
-        error: function (request, error) {
-            console.log(request + ":" + error);
-        }
-    });
-    
-    $.ajax({
-        method: "GET",
-        //dataType: "json",
-        dataType: 'json',
-        crossDomain: true,
-        url: "http://localhost:3000/agenda_location",
-       
-        success: function (response) {
-           
-            console.log(response);
-            
-            loadData(response);
-        },
-        
         error: function (request, error) {
             console.log(request + ":" + error);
         }
@@ -67,34 +40,23 @@ $(document).ready(function(){
 
 
 
-function createGallery(baseSrc){
-    var gal = "";
-    for(var i = 1; i<=6; i++){
-      var iLink = baseSrc + i + ".jpg";
-      gal += '<div class="col-xs-6 col-sm-4 col-md-2 col-lg-2"><a href="'+iLink+'" data-lightbox="gallery"><img src="'+iLink+'"></a></div>';
-    }
-    $("#gallery_loc").append(gal);
-}
-
-
-
-function loadData(json) {
+/*function loadData(json) {
 
     console.log(json);
     var el = "";
     
     for (var i = 0; i < json.length; i++){
         
-        var servLink = '../services/service.html?id=' + json[i].idservizio;
+        var locLink = '../location/location.html?id=' + json[i].idlocation;
+        var persLink = '../people/person.html?id=' + json[i].id;
         
-        el += '<li><a href="'+servLink+'">'+json[i].servicesname+'</a><p>Orari di servizio:<br><div>'+json[i].orario+'</div></li>';
+        el += '<li><a href="'+locLink+'">'+json[i].locationname+'</a><ul><li><small><a href="'+persLink+'">'+json[i].name+'</a></small><br>Orari di servizio:<br><div>'+json[i].orario+'</div></li></ul></li>';
 
     }
     
     $("#agendaList").append(el);
-    
-
-}
+   
+}*/
     
 
     
--- public/assets/js/persona.js
+++ public/assets/js/persona.js
@@ -1,21 +1,21 @@
 var page_title;
-var serv_title;
+var pers_title;
 var contenuto;
 var image_url;
 
 $(document).ready(function(){
     
     page_title = $("title");
-    serv_title = $("#title_serv");
-    contenuto = $("#contenuto_serv");
-    image_url = $("#image_serv");
+    serv_title = $("#title_per");
+    contenuto = $("#contenuto_per");
+    image_url = $("#image_per");
      console.log(URL.id);
     $.ajax({
         method: "GET",
         //dataType: "json",
         dataType: 'json',
         crossDomain: true,
-        url: "http://localhost:3000/service/"+URL.id,
+        url: "http://localhost:3000/person/"+URL.id,
         data: {
             id: URL.id //SEND THE ID OF THE SERVICE TO THE SERVER TO RETRIVE ONLY THOSE DATA
         },
@@ -27,7 +27,7 @@ $(document).ready(function(){
             page_title.text(response.name);
             serv_title.text(response.name);
             contenuto.html(response.descrizione);
-            image_url.attr("src", response.img);
+            image_url.attr("src", response.img2);
             
         },
         error: function (request, error) {
@@ -40,7 +40,7 @@ $(document).ready(function(){
         //dataType: "json",
         dataType: 'json',
         crossDomain: true,
-        url: "http://localhost:3000/agenda?page=people&spec="+URL.id,
+        url: "http://localhost:3000/agenda?page=services&spec="+URL.id,
        
         success: function (response) {
            
@@ -54,28 +54,69 @@ $(document).ready(function(){
         }
     });
     
+    $.ajax({
+        method: "GET",
+        //dataType: "json",
+        dataType: 'json',
+        crossDomain: true,
+        url: "http://localhost:3000/all?table=people",
+       
+        success: function (response) {
+           
+            console.log(response);
+            
+            createCarousel(response);
+        },
+        
+        error: function (request, error) {
+            console.log(request + ":" + error);
+        }
+    });
+    
     
 });
 
 
 
+function createCarousel(json){
+    var caro = "";
+    for (var i = 0; i < json.length; i++){
+        if(json[i].id != URL.id){
+            var link = "persona.html?id="+json[i].id;
+        
+            caro += '<div class="item"><a href="'+link+'" class="overlay-wrapper"><img src="'+json[i].img1+'" class="img-responsive underlay" width="200px"><span class="overlay"><span class="overlay-content"> <span class="h4">'+''/*Manca la qualifica del tipo*/+'</span> </span></span></a><div class="item-details bg-noise"><h4 class="item-title"><a href="'+link+'">'+json[i].name+'</a></h4></div></div>';
+        }
+    }
+    $("#other_people").append(caro);
+}
+
+
+
 function loadData(json) {
 
-    console.log(json);
+    if(URL.id != 1)
+        $("#serv_svolti").html('<h3 class="block-title">Servizi svolti</h3>');
     var el = "";
     
     for (var i = 0; i < json.length; i++){
+        if(i==0 || json[i].id != json[i-1].id){
+            var servLink = '../services/service.html?id=' + json[i].id;
         
-        var locLink = '../location/location.html?id=' + json[i].idlocation;
-        var persLink = '../people/person.html?id=' + json[i].id;
+            el += '<li><a href="'+servLink+'">'+json[i].name+'</a><ul>';
         
-        el += '<li><a href="'+locLink+'">'+json[i].locationname+'</a><ul><li><small><a href="'+persLink+'">'+json[i].name+'</a></small><br>Orari di servizio:<br><div>'+json[i].orario+'</div></li></ul></li>';
+            for(var j = i; j < json.length; j++){
+                if(json[i].id == json[j].id){
+                    var locLink = '../location/location.html?id=' + json[j].idlocation;
 
-        
+                    el += '<li><small><a href="'+locLink+'">'+json[j].locationname+'</a></small><br>Orari di servizio:<br><div>'+json[j].orario+'</div></li>';
+                }
+            }
+        }
+        el += '</ul></li>';
         
     }
     
-    $("#agendaList").append(el);
+    $("#agenda").append(el);
     
 
 }
