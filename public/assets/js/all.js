b3f283cedf2954e314f17851d8ba68d5e6fd0c6c
diclementef92 <diclementef92@gmail.com>
2018-06-15 12:42:21 +0200

    pagina all.html resa dinamica anche per all_people

--- public/assets/js/all.js
+++ public/assets/js/all.js
@@ -5,7 +5,7 @@ var image_url;
 $(document).ready(function(){
     console.log("READY");
     
-    page_title = $("#title");
+    page_title = $("title");
     list = $("#content");
     console.log(URL.table);
     
@@ -20,8 +20,10 @@ $(document).ready(function(){
         success: function(response) {
             console.log(response.name);
             
-            page_title.text(response.name);
-            
+            //page_title.text(response.name);
+            if(URL.table=="people"){
+                console.log(response.qualifica);
+            }
             loadData(response);
         },
         error: function(request, error){
@@ -36,26 +38,65 @@ $(document).ready(function(){
 function loadData(json){
     console.log(json);
     var el="";
-    el += '<div class="services block block-bg-gradient"><div class="container">'
+    el += '<div class="services block block-bg-gradient-border-bottom"><div class="container">'
+    
     var ph="";
-    var link="services/service.html?id=";
-    if(URL.table!="services"){
-       link="location/location.html?id=";
-        ph="1.jpg";}
-    for(var i = 0; i< json.length; i++){
-        var locLink = link + json[i].id;
-        var locImg = json[i].img;
-        el += '<div class="row"><div class="col-md-7"><a href="' + locLink + '"><img  class="img-fluid rounded mb-3 mb-md-0" src="'+ locImg + ph+'" height="60%" width="60%" alt="' + json[i].name + '></a></div><div class="col-md-5"><h3><b>' +json[i].name + '</b></h3> <a class="btn btn-primary" href="' + locLink + '">Scopri di più</a></div></div>';
-        
-        if(i != (json.length - 1)){
-            el += '<hr>'; 
-        }      
-
+    var link="";
+    
+    if(URL.table=="services"){
+        link="services/service.html?id=";
+        page_title.text("All services");
     }
     
-    el += '</div></div>';
+    if(URL.table=="location"){
+       link="location/location.html?id=";
+        ph="1.jpg";
+        page_title.text("All locations");
+    }
+    
+    if(URL.table=="people"){
+        link="people/persona.html?id=";
+        page_title.text("All people");
+    }
+    
+    if(URL.table=="services" || URL.table=="location" ){
+        for(var i = 0; i< json.length; i++){
+            
+            var itemLink = link + json[i].id;
+            var itemImg = json[i].img;
+            el += '<div class="row"><div class="col-md-7"><a href="' + itemLink + '"><img  class="img-fluid rounded mb-3 mb-md-0" src="'+ itemImg + ph+'" height="60%" width="60%" alt="' + json[i].name + '"></a></div><div class="col-md-5"><h3><b>' +json[i].name + '</b></h3> <a class="btn btn-primary" href="' + itemLink + '">Scopri di più</a></div></div>';
         
-    $("#content").append(el);
+            if(i != (json.length - 1)){
+            el += '<hr>'; 
+            }      
+        }
+        el += '</div></div>';
+        
+    }
+    
+    if(URL.table=="people"){
+        el += '<div class="row"><img src="../assets/img/people/peoples.png" width="100%" height="20%"></div> <div class="row"><hr><h1>Semplicemente Noi</h1></div><h4>Membri associazione in ordine alfabetico</h4></div></div><div class="showcase block block-border-bottom-grey"><div class="container">';
+        
+        for(var i = 0; i< json.length; i++){
+
+            var itemLink = link + json[i].id;
+            var itemImg = json[i].img1;
+            el += '<div class="row"><div class="col-md-6"><div class="col-md-6"><a href="' + itemLink + '"><img src="' + itemImg + '" width="50%" height="50%"></a></div><div class="col-md-6"><h3><a href="' + itemLink +'">' + json[i].name + '</a></h3><p>' +'</p></div></div></div>';
+            //problema non legge la qualifica
+            
+            if(i != (json.length - 1)){
+            el += '<hr>'; 
+            }      
+        }
+        
+        el += '</div></div>';
+    
+    }
+
+    
+    $("#content").append(el);   
+    
+    
 }
 
    
